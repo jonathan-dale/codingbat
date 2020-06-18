7708a4b79e8c13ab6c4e10691c78697c39868ceb
jonathan-dale <53344+gb69@users.noreply.github.com>
2020-06-18 08:29:15 -0400

    Started on Array-1

---
+++ java/Array-1/makePi.java
@@ -0,0 +1,12 @@
+/**
+ * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+ */
+
+
+public class makePi {
+
+    public int[] makePi() {
+        int[] arr = new int[] {3, 1, 4};
+        return arr;
+    }
+}
---
+++ java/Array-1/sum3.java
@@ -0,0 +1,14 @@
+/**
+ * Given an array of ints length 3, return the sum of all the elements.
+ */
+
+
+public class sum3 {
+    public int sum3(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
